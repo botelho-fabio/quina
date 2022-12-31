cc8b4a0f08e9610dfc8f4e59d42dfa89aff6ea09
Botelho <120726287+botelho-fabio@users.noreply.github.com>
2022-12-31 20:19:44 -0300

    Brazilian Lottery known as Quina

--- quina.py
+++ quina.py
@@ -16,4 +16,4 @@ for a in range(0, 80):
                             print(str(jogo) + ' ' + str(a+1) + ' ' + str(b+1) +
                                   ' ' + str(c+1) + ' ' + str(d+1) + ' ' + str(e+1))
 
-print("Percorri no processo filho 24.040.016 combinações que correspondem a combinação de 80, 5 a 5")
+print("Percorri 24.040.016 combinações que correspondem a combinação de 80, 5 a 5")
